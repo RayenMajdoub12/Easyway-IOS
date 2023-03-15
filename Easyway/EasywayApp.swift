2bc0ad272692cdb18b97c6c1e3bafafee3b60d76
Rayen Majdoub <61800578+RayenMajdoub12@users.noreply.github.com>
2023-03-15 16:31:50 +0100

    GetStarted page

--- Easyway/EasywayApp.swift
+++ Easyway/EasywayApp.swift
@@ -13,8 +13,7 @@ struct EasywayApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+           GetStarted()
         }
     }
 }
