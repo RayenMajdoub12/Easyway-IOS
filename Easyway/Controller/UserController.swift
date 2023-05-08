a0838c2dd9bfb6b8dd1fb1f4b014456e98d3afa3
Rayen Majdoub <61800578+RayenMajdoub12@users.noreply.github.com>
2023-05-08 11:40:31 +0100

    profile pic bug

--- Easyway/Controller/UserController.swift
+++ Easyway/Controller/UserController.swift
@@ -34,6 +34,7 @@ class UserController {
                     UserDefaults.standard.set(username, forKey: "username")
                     UserDefaults.standard.set(email, forKey: "email")
                     UserDefaults.standard.set(role, forKey: "role")
+                    UserDefaults.standard.set("profilepic", forKey: "profilepic")
                         
                     completion(.success(token))
                 } else {
